9d3aa2d2a31f4f67808d1cb5ea9c386a07eb5d71
gabeart10 <millergabe10@gmail.com>
2016-03-18 20:04:07 -0400

    save

--- go/src/eightball/main.go
+++ go/src/eightball/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-	"os"
-	c"github.com/skilstak/go/colors"
-	"time"
+	"fmt"
+	c "github.com/skilstak/go/colors"
 	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
-var eastereggs 
+var answers = []string{
+	"Yes",
+	"No",
+	"Maybe",
+}
+
+var easterEgg1 = []string{"kill", "Well Then"}
+var easterEgg2 = []string{"love", "Love you too ;)"}
+var responses = [][]string{easterEgg1, easterEgg2}
 
 func main() {
-	s1 = rand.NewSource(time.Now().UnixNano())
-	r1 = rand.New(s1)
-
+	for true {
+		egg := 0
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(c.Green + "What do you want to know?\n" + c.Reset)
+		text, _ := reader.ReadString('\n')
+		for _, easterEgg := range responses {
+			if strings.Contains(text, easterEgg[0]) {
+				fmt.Println(c.Red + easterEgg[1] + c.Reset)
+				egg = 1
+				reader.ReadString('\n')
+				fmt.Print(c.Clear)
+			}
+		}
+		if egg == 0 {
+			num := r1.Intn(3)
+			fmt.Println(c.Yellow + answers[num] + c.Reset)
+			reader.ReadString('\n')
+			fmt.Print(c.Clear)
+		}
+	}
+}
